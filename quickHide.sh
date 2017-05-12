0803eb35547365ff74e048808b31f6b5e3511641
Vadim Brik <Brik99brik@gmail.com>
2017-05-12 14:07:59 +0600

    Update quickHide.sh

--- quickHide.sh
+++ quickHide.sh
@@ -5,19 +5,23 @@ key="$1"
 hideDesktop () {
     defaults write com.apple.finder CreateDesktop false
     killall Finder
+    osascript -e 'display notification "Desktop is disabled" with title "quickHide.sh" sound name "Sound"'
 }
 
 showDesktop () {
     defaults write com.apple.finder CreateDesktop true
     killall Finder
+    osascript -e 'display notification "Desktop is enabled" with title "quickHide.sh" sound name "Sound"'
 }
 
 hideAllIcons () {
     chflags hidden ~/Desktop/*
+    osascript -e 'display notification "Desktop icons hidden" with title "quickHide.sh" sound name "Sound"'
 }
 
 showAllIcons () {
     chflags nohidden ~/Desktop/*
+    osascript -e 'display notification "Desktop icons are displayed" with title "quickHide.sh" sound name "Sound"'
 }
 
 case $key in
